c0bc692e94012651266ba7bf1254f92f953921f4
NightRider0098 <39752890+Nightrider0098@users.noreply.github.com>
2020-10-29 16:59:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,41 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState, useEffect } from "react";
+import { render } from "react-dom";
 import '../styles/App.css';
 
 class App extends Component {
-    render() {
 
-        return(
-            <>
-               
-            </>
-        )
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      time:'',
+      intervalID:''
     }
+    this.updatetime = this.updatetime.bind(this)
+  }
+
+  componentDidMount() {
+    this.setState({
+      time: (new Date).toLocaleTimeString(), intervalID: setInterval(() => {
+        this.updatetime();
+      }, 1000)
+    })
+  }
+
+  updatetime() {
+    this.setState({ time: (new Date).toLocaleTimeString() })
+  }
+  componentWillUnmount() {
+    clearInterval(this.state.intervalID)
+  }
+  render() {
+    return (
+      <div>
+        <h3 id='time'>{this.state.time}</h3>
+      </div>
+    )
+  }
 }
 
+export default App
 
-export default App;
