434c3c045751aeefce37255898c3c9ada0ccd2f0
Nightrider0098 <hitesh08011999@gmail.com>
2020-10-29 15:27:46 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,36 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState, useEffect } from "react";
+import { render } from "react-dom";
 import '../styles/App.css';
 
 const App = () => {
+
+  let cDate = new Date;
+  let renderString = "" + cDate.getHours() % 12 + ":" + cDate.getMinutes() + ":" + cDate.getMilliseconds() % 100 + " "
+  if (cDate.getHours() % 12)
+    renderString += 'AM'
+  else
+    renderString += 'PM'
+  var [time, setTime] = useState(renderString);
+
+
+  let setintervalID;
+
+  useEffect(() => {
+    let setintervalID = setInterval(() => {
+      const cDate = new Date;
+      let renderString = "" + cDate.getHours() % 12 + ":" + cDate.getMinutes() + ":" + cDate.getMilliseconds() % 100 + " "
+      if (cDate.getHours() % 12)
+        renderString += 'AM'
+      else
+        renderString += 'PM'
+      setTime(renderString)
+      // alert(cDate+renderString+'ites')
+    }, 1000);
+
+    return () => clearInterval(setintervalID);
+  })
   return (
-    <></>
+    <div>{time}</div>
   )
 }
 
